d8587af98acf160eb72114f23e238f541967e8eb
ljohny <ljohny@mathworks.com>
2025-12-19 16:46:28 +0100

    adding initial version of trial

---
+++ main.py
@@ -0,0 +1,16 @@
+from fastembed import TextEmbedding
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+from openai import OpenAI
+
+client = OpenAI()
+
+
+
+
+def main():
+    print("Hello from rag!")
+
+
+if __name__ == "__main__":
+    main()
